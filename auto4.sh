f87ae2b26fb06995538f3e94b617adc07198cb04
chrisWalker11 <51098965+chrisWalker11@users.noreply.github.com>
2020-07-02 09:39:03 -0400

    Update auto4.sh

--- auto4.sh
+++ auto4.sh
@@ -3,18 +3,28 @@ echo "whats your name?"
 read a
 sudo mkdir -p /usr/coursework/{FacultyNotes,LectureSlides,StudentWorkspace/{BetsyAfiles,$a}}
 sudo chown betsya /usr/coursework/StudentWorkspace/BetstyAfiles
+
 echo "first screenshot"
+echo " "
+echo " "
 ls -al /usr/coursework/StudentWorkspace/
-stop 10s && echo "#2"
+
 sudo chown suej /usr/coursework/LectureSlides
+echo " "
+echo " "
+echo " "
+
 ls -al | grep -i lec /usr/coursework
-stop 10s && echo "screenshot 3"
+
 sudo chmod o-rx /usr/coursework/StudentWorkspace/BetsyAfiles
 sudo chmod g-rx /usr/coursework/StudentWorkspace/BetsyAfiles
+echo " "
+echo " "
+echo ""
 ls -al | grep -i bet /usr/coursework/StudentWorkspace
-stop 10s
+
 sudo chmod o-x /usr/coursework/LectureSlides
 sudo chmod g-x /usr/coursework/LectureSlides
+echo " " 
+echo " "
 ls -al | grep -i lec /usr/coursework/LectureSlides
-stop 10s && echo "screenshot"
-
